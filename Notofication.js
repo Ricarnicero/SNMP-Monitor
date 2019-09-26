055c2a291722e8f1e6e065102106c26812cae1b8
Ricardo Torres <ricardots211@gmail.com>
2019-09-26 15:30:44 -0500

    AddingNotifications

--- Notofication.js
+++ Notofication.js
@@ -2,10 +2,23 @@ const axios = require('axios')
 
 class Notification{
     sendNotification(event,val1,val2,val3){
-        axios.post('https://maker.ifttt.com/trigger/'+event+'/with/key/d2sCAW4Y-k6OnKgv9xZ3xyiCgWu5FRQfpr2xfEjesw3', {"value1":val1,"value2":val2,"value3":val3})
+      axios.post('https://maker.ifttt.com/trigger/notificar/with/key/d2sCAW4Y-k6OnKgv9xZ3xyiCgWu5FRQfpr2xfEjesw3', {"value1":val1,"value2":val2,"value3":val3})
         .catch((error) => {
           console.error(error)
         })
+        axios.post('https://maker.ifttt.com/trigger/facebook/with/key/d2sCAW4Y-k6OnKgv9xZ3xyiCgWu5FRQfpr2xfEjesw3', {"value1":val1,"value2":val2,"value3":val3})
+        .catch((error) => {
+          console.error(error)
+        })
+        axios.post('https://maker.ifttt.com/trigger/sendMessage/with/key/d2sCAW4Y-k6OnKgv9xZ3xyiCgWu5FRQfpr2xfEjesw3', {"value1":val1,"value2":val2,"value3":val3})
+        .catch((error) => {
+          console.error(error)
+        })
+        axios.post('https://maker.ifttt.com/trigger/TrapDetected/with/key/d2sCAW4Y-k6OnKgv9xZ3xyiCgWu5FRQfpr2xfEjesw3', {"value1":val1,"value2":val2,"value3":val3})
+        .catch((error) => {
+          console.error(error)
+        })
+        
     }
 }
 
